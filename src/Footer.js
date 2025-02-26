c26824c67443f2a5c85ae9a7c7bd45525bfd3289
JeFFunique <kayzenpro935@gmail.com>
2025-02-26 15:01:18 +0100

    latest commit

--- src/Footer.js
+++ src/Footer.js
@@ -8,12 +8,16 @@ function Footer() {
 <img src={logo}></img>
 </div>
 <div className='map'>
+    <div className='part'>
     <p><Link to="/">Home</Link></p>
     <p><Link to="/about">About</Link></p>
     <p><Link to="/menu">Menu</Link></p>
+    </div>
+    <div className='part'>
     <p><Link to="/booking">Reservations</Link></p>
     <p><Link to="/order-online">Order Online</Link></p>
     <p><Link to="/login">Login</Link></p>
+    </div>
 </div>
 <div className='contact'>
     <h2>CONTACT US</h2>
